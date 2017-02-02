a8181c8a26ee9aa34d91e127dc9a346a40427ec3
XZS <d.f.fischer@web.de>
2017-02-03 00:27:25 +0100

    Exemplify usage
    It is adapted from the example provided amongst in the
    gnome-shell-extensions maintained by the GNOME project itself. [1]
    
    As of this commit, it does not use any features of the not yet existing
    tool. They will gradually grow together, while the usage example, will
    also serve as a testbed.
    
    [1]: https://github.com/GNOME/gnome-shell-extensions/tree/master/extensions/example

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # waftool for gnome-shell extensions
 
 This tool teaches [Waf](http://waf.io) to install gnome-shell extensions.
+
+
+## Usage
+
+[The included example](example/wscript) shows how to use it in a wafscript.
---
+++ example/extension.js
@@ -0,0 +1,28 @@
+const St = imports.gi.St;
+const Mainloop = imports.mainloop;
+const Main = imports.ui.main;
+
+function _showHello() {
+  let label = new St.Label({ text: "Hello, world!" });
+  let monitor = Main.layoutManager.primaryMonitor;
+  global.stage.add_actor(label);
+  label.set_position(Math.floor (monitor.width / 2 - label.width / 2), Math.floor(monitor.height / 2 - label.height / 2));
+  Mainloop.timeout_add(3000, function () { label.destroy(); });
+}
+
+function init() {
+}
+
+let signalId;
+
+function enable() {
+  Main.panel.actor.reactive = true;
+  signalId = Main.panel.actor.connect('button-release-event', _showHello);
+}
+
+function disable() {
+  if (signalId) {
+    Main.panel.actor.disconnect(signalId);
+    signalId = 0;
+  }
+}
