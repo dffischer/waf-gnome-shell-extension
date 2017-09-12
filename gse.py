c6abfd9755f505497e9014f19905aeff6d7140fd
XZS <d.f.fischer@web.de>
2017-09-12 19:59:58 +0100

    Install files

---
+++ gse.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+"""
+Install gnome-shell extensions.
+
+The functions herein center around a feature called 'gse' which installs all
+specified source files into a uuid-named directory for the shell to find it.
+
+    def configure(cnf):
+        cnf.load("gse")
+
+    def build(bld):
+        bld(features="gse", uuid="some@extension", source="prefs.js")
+"""
+
+from waflib.TaskGen import feature, before_method
+from waflib.Errors import WafError
+from os.path import join
+
+def configure(cnf):
+    cnf.env.HOME = cnf.environ['HOME']
+
+@feature("gse")
+@before_method('process_source')
+def process_gse(gen):
+    sources = gen.to_nodes(getattr(gen, 'source', []))
+    gen.source = []  # Suppress further processing.
+
+    uuid = getattr(gen, "uuid", None)
+    if not uuid:
+        raise WafError("missing uuid in {}".format(self))
+
+    gen.bld.install_files(files=sources, dest=join(gen.env.HOME,
+        ".local", "share", "gnome-shell", "extensions", uuid))
