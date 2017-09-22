14a26c944aeb4f542447407d835b996d5615de99
XZS <d.f.fischer@web.de>
2017-09-22 21:23:26 +0100

    Utilize a schema in the example

--- example/extension.js
+++ example/extension.js
@@ -1,9 +1,10 @@
 const St = imports.gi.St;
 const Mainloop = imports.mainloop;
 const Main = imports.ui.main;
+const settings = imports.misc.extensionUtils.getCurrentExtension().imports.convenience.getSettings();
 
 function _showHello() {
-  let label = new St.Label({ text: "Hello, world!" });
+  let label = new St.Label({ text: settings.get_string('hello-text') || "Hello, world!" });
   let monitor = Main.layoutManager.primaryMonitor;
   global.stage.add_actor(label);
   label.set_position(Math.floor (monitor.width / 2 - label.width / 2), Math.floor(monitor.height / 2 - label.height / 2));
